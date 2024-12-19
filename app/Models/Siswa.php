834cfc23ef741c54cec9bc6bf1b6d9c9ebbc0adc
ahmaddevatajulm <ahmaddevatajulmuluk@gmail.com>
2024-12-19 19:09:50 +0700

    umur di siswa done

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Siswa extends Model
 {
     use HasFactory;
     protected $table = 'siswa';
-    protected $fillable = ['name', 'nis', 'kelas', 'umur', 'gender', 'photo'];
+    protected $fillable = ['name', 'nis', 'kelas', 'tanggal_lahir', 'gender', 'photo'];
+
+    public function getUmurAttribute()
+    {
+        return Carbon::parse($this->tanggal_lahir)->age;
+    }
 }
